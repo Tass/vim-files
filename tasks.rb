237fd48e8268aea6ddc6328dcba1223c7d713037
Simon Hafner <hafnersimon@gmail.com>
2010-04-08 23:13:51 +0200

    added tasks.rb file for install

---
+++ tasks.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env nake
+
+exp = File.method(:expand_path)
+
+task(:symlinks) do
+  File.symlink(exp.call("vimrc"), exp.call("~/.vimrc"))
+  File.symlink(exp.call("."), exp.call("~/.vim")) unless File.exists?(exp.call("~/.vim"))
+end
+
+task(:submodules) do
+  system "git submodule init"
+end
+
+task(:update) do
+  system "git submodule update"
+end
+
+task(:install, :symlinks, :submodules, :update)
